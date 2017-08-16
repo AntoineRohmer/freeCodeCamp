1ee766fc9d591f340b410087da5e8b8593a014c2
Antoine Rohmer <antoine.rohmer96@gmail.com>
2017-08-16 16:09:37 -0400

    Create 02-record-collection.js

---
+++ Advanced-Algorithms/02-record-collection.js
@@ -0,0 +1,45 @@
+var collection = {
+    "2548": {
+      "album": "Slippery When Wet",
+      "artist": "Bon Jovi",
+      "tracks": [ 
+        "Let It Rock", 
+        "You Give Love a Bad Name" 
+      ]
+    },
+    "2468": {
+      "album": "1999",
+      "artist": "Prince",
+      "tracks": [ 
+        "1999", 
+        "Little Red Corvette" 
+      ]
+    },
+    "1245": {
+      "artist": "Robert Palmer",
+      "tracks": [ ]
+    },
+    "5439": {
+      "album": "ABBA Gold"
+    }
+};
+
+var collectionCopy = JSON.parse(JSON.stringify(collection));
+
+function updateRecords(id, prop, value) {
+  if (value === "") {
+    delete collection[id][prop];
+  }
+  else if (prop === "artist") {
+      collection[id][prop] = value; 
+  }
+  else {
+    if (!collection[id].hasOwnProperty(prop)) {
+      collection[id][prop] = [value];
+    }
+    else {
+      collection[id][prop].push(value);
+    }
+  }
+  return collection;
+}
